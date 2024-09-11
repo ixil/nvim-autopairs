9a31cab3593e77f7e1a0b4a6b2f04a3956560e7d
ixil <oliver.r.harley@gmail.com>
2024-09-11 02:50:11 +0200

    wip: Start trying to merge positions and simplify based on runs / adjacency

--- lua/nvim-autopairs/fastwrap.lua
+++ lua/nvim-autopairs/fastwrap.lua
@@ -7,6 +7,7 @@ local default_config = {
     map = '<M-e>',
     chars = { '{', '[', '(', '"', "'" },
     pattern = [=[[%'%"%>%]%)%}%,%`]]=],
+    interpattern = [=[[(%s%w)]]=],
     end_key = '$',
     end_is_end = true, -- always treat end_key as eol
     avoid_move_to_end = true, -- choose your move behaviour for non-alphabetical end_keys' 
@@ -43,45 +44,123 @@ function M.getchar_handler()
     return nil
 end
 
+--- At position col in the line, returns true if the closing pair should be offset due to the previous character being a closing bracket, or quotes.
+function M.should_offset(line, col, char, prev_char)
+    --CHECK: Not sure the is_in_quotes makes sense here?
+    return utils.is_quote(char) or ( utils.is_close_bracket(char) and utils.is_in_quotes(line, col, prev_char))
+end
+
+M.key_amongst = {
+    left = {},    -- *a* b c    d
+    between = {}, -- a *b* c    d
+    singular = {}, -- a b c    *d*
+    right = {},   -- a b *c*    d
+    -- )]
+
 M.show = function(line)
     line = line or utils.text_get_current_line(0)
     log.debug(line)
     local row, col = utils.get_cursor()
     local prev_char = utils.text_cusor_line(line, col, 1, 1, false)
-    local end_pair = ''
+    local closing_pair = ''
+
+    local nextKey = function()
+        local index = 0
+        local iter = function()
+            index = index + 1
+            return config.keys:sub(index, index)
+        end
+        return iter
+    end
+
     if utils.is_in_table(config.chars, prev_char) then
         local rules = npairs.get_buf_rules()
         for _, rule in pairs(rules) do
             if rule.start_pair == prev_char then
-                end_pair = rule.end_pair
+                closing_pair = rule.end_pair
             end
         end
-        if end_pair == '' then
+        if closing_pair == '' then
             return
         end
         local list_pos = {} --holds target locations
         local index = 1
         local str_length = #line
-        local offset = -1
-        for i = col + 2, #line, 1 do
-            local char = line:sub(i, i)
-            local char2 = line:sub(i - 1, i)
-            if string.match(char, config.pattern)
-                or (char == ' ' and string.match(char2, '%w'))
-            then
-                local key = config.keys:sub(index, index)
-                index = index + 1
-                if not config.manual_position and (
-                        utils.is_quote(char)
-                        or (
-                            utils.is_close_bracket(char)
-                            and utils.is_in_quotes(line, col, prev_char)
-                        )
-                    )
-                then
+        -- local interpattern = [=[(%s%w)]=]
+        -- from cursor_0 to end of line, check if previous char is ' ', "%w", or current in in pattern
+        local i = col
+        local line_matches = {}
+        while i < #line+1  do --CHECK: off-by one?
+            local offset = -1
+            local sii, eii, sm = string.find(line:sub(i+1, -1), config.interpattern, i, false)
+            local spi, epi, pm = string.find(line:sub(i+1, -1), config.pattern, i, false)
+            i = math.min(eii or #line, epi or #line) -- skip ahead to next match
+
+            if config.manual_position then 
+                -- TODO: do something with this?
+            end
+
+            -- note this is unsorted
+            if spi and sii ~= spi then
+                -- TODO: is len useful?
+                table.insert(line_matches, {(col + spi + offset), {match = pm, len = epi-spi, offset = offset}})
+            elseif sii then
+                -- Move after a quote/closing bracket / being inside a quote?
+                --TODO: Should this go before or after the narrowing of locations
+                if not config.manual_position and M.should_offset(line, col+sii, sm[1], line:sub(col+sii, col+sii)) then
                     offset = 0
                 end
+                table.insert(line_matches, {(col + sii+offset), {match = sm, len = eii-sii, offset = offset}})
+            end
+        end
+        table.insert(line_matches, {str_length, {match = '', len = 0, offset=0}}) -- always add end of line
 
+        -- Sort matches
+        local line_pos = {}
+        for n in pairs(line_matches) do table.insert(line_pos, n) end
+        table.sort(line_pos)
+
+        local adjacent = false
+        local span = 0 
+        for j,i in ipairs(line_pos) do
+            local m = line_matches[line_pos[i]]
+            prev_char = line:sub(line_pos[i]-1, line_pos[i])
+
+            if last + 1 == line_pos[i] then
+                adjacent = true
+            else
+                adjacent = false
+                if run then
+                    list_pos[last_item].key_position = key_amongst.right
+                end
+                run = false
+            end
+
+            if line_matches[last].match == m.match and adjacent then
+                span = span + 1
+            -- continue
+            else
+                span = 0
+                if adjacent then
+                else
+                end
+            if last_match and last_match.col+1 == i and not adjacent then
+                --adjacent
+                adjacent = true
+                last_item = list_pos
+                table.insert(list_pos, {col = line_pos[i]+m.offset, key = nextKey(), pos = end_pos, char = m.match, span = span})
+            end
+
+            run = adjacent
+            last = line_pos[i]
+            end
+        end
+
+            -----------------------------------
+            -- TODO: extract into function
+                -----------------------------------
+
+                -- TODO: Move the key logic outside, this also duplicates the manual end_key insertion
                 if config.manual_position and i == str_length then
                     key = config.end_key
                 end
@@ -90,11 +169,11 @@ M.show = function(line)
                     list_pos,
                     { col = i + offset, key = key, char = char, pos = i }
                 )
-            end
         end
         log.debug(list_pos)
 
         local end_col, end_pos
+    -- TODO: the non-manual_position?
         if config.manual_position then
             end_col = str_length + offset
             end_pos = str_length
@@ -102,6 +181,7 @@ M.show = function(line)
             end_col = str_length + 1
             end_pos = str_length + 1
         end
+
         -- add end_key to list extmark
         if #list_pos == 0 or list_pos[#list_pos].key ~= config.end_key then
             table.insert(
@@ -130,7 +210,7 @@ M.show = function(line)
                     vim.print("Run to end!")
                     -- M.highlight_wrap({pos = pos.pos, key = config.end_key}, row, col, #line, whitespace_line)
                     local move_end_key = (not config.avoid_move_to_end and char == string.upper(config.end_key))
-                    M.move_bracket(line, pos.col+1, end_pair, move_end_key)
+                    M.move_bracket(line, pos.col+1, closing_pair, move_end_key)
                     break
                 end
                 local hl_mark = {
@@ -139,15 +219,15 @@ M.show = function(line)
                 }
                 if config.manual_position and (char == pos.key or char == string.upper(pos.key)) then
                     M.highlight_wrap(hl_mark, row, col, #line, whitespace_line)
-                    M.choose_pos(row, line, pos, end_pair)
+                    M.choose_pos(row, line, pos, closing_pair)
                     break
                 end
                 if char == pos.key then
-                    M.move_bracket(line, pos.col, end_pair, false)
+                    M.move_bracket(line, pos.col, closing_pair, false)
                     break
                 end
                 if char == string.upper(pos.key) then
-                    M.move_bracket(line, pos.col, end_pair, true)
+                    M.move_bracket(line, pos.col, closing_pair, true)
                     break
                 end
             end
@@ -180,12 +260,18 @@ M.choose_pos = function(row, line, pos, end_pair)
     end, 10)
 end
 
+M.existing_end_bracket = function(line, i, end_pair)
+    -- Determine if at location i within the line end_pair already exists
+ return line:sub(i, end_pair:len()) == end_pair
+end
+
 M.move_bracket = function(line, target_pos, end_pair, change_pos)
     log.debug(target_pos)
     line = line or utils.text_get_current_line(0)
     local row, col = utils.get_cursor()
     local _, next_char = utils.text_cusor_line(line, col, 1, 1, false)
     -- remove an autopairs if that exist
+    -- FIXME: should be redundant if already fixed elsewhere
     if next_char == end_pair then
         line = line:sub(1, col) .. line:sub(col + 2, #line)
         target_pos = target_pos - 1
